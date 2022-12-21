3e08827cb672540bf0ba0ada0259502f8cbd8d50
Daniel <danieel47896321@gmail.com>
2022-12-21 16:04:46 +0200

    changed themes to orange. added floating button to add and remove note.

--- app/src/main/java/com/example/mynotes/MainActivity.kt
+++ app/src/main/java/com/example/mynotes/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.mynotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userSearch : EditText
+    private lateinit var recyclerView : RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+    private fun init () {
+        userSearch = findViewById<EditText>(R.id.userSearch)
+        recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
     }
 }
